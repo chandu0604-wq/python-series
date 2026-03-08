6fd086ac9cb7e96c6b8c7b15ffb568b678423bfe
Hruthik D Gowda <chandud961@gmail.com>
2026-03-08 23:01:23 +0530

    Add files via upload

---
+++ git upload.py
@@ -0,0 +1,49 @@
+# student={}
+
+# while True:
+#     print("A-Enter student: ")
+#     print("B-Enter update: ")
+#     print("C-search: ")
+#     print("D-display: ")
+    
+#     user=input("Enter the choice: ")
+    
+#     if user=="A":
+#         name=input("Enter the student name: ")
+#         marks=int(input("Enter the marks: "))
+#         student[name]=marks
+#     elif user=="B":
+#         name=input("Enter the student name: ")
+#         marks=int(input("Enter the marks: "))
+#         student[name]=marks
+#     elif user=="C":
+#         name=input("Enter the student name: ")
+#         if name in student:
+#             print(name,"marks: ",student[name])
+#         else:
+#             print("Student not found.")
+#     elif user=="D":
+#         for name, marks in student.items():
+#             print(name, "marks: ", marks)
+#     else:
+#         print("Invalid choice. Please try again.") 
+#     print(student) 
+
+
+
+# print("A">"a") 
+
+# print("A"<"a")
+
+# if "A" > "a":
+#     print("A is greater than a")
+# else:
+#     print("A is less than a")
+
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1, 10, 2):
+#     print(i)
+
